1ce152224d6aedae8f639e9fa87d2b98584fda79
ravish0007 <ravish.nfs@gmail.com>
2022-05-05 11:40:03 +0530

    update setup_git_user.sh:  uncomment install command

--- setup_git_user.sh
+++ setup_git_user.sh
@@ -3,7 +3,7 @@
 MY_PUB_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC4Uxj/vtfpklZwXJs06BYIoJEKPpJhF3dNb055psfDob95/iCPSZ4mKbxsBTII4l7uxF3BotFQMqmHRjkfa9xSOU2zo54kYkGB0gaCsXfdJNbSIGRMe/2bZ3Qwj4Px6vq33E64guaZQmjnf9a/CwJNAWwAeBxT4dW04wNBjQ4qFdIRLWcvghS4pjO8L5Nj4LidjIaRpVCVtUzTnzpppTof7U6TAI4nIF76jgTVvweBl29MJn2nXApSTMIiM7JOkVOe5KLZT7rWAUsaxoosmfcinQ2Y/SLHtTvp3i6AGWc2aqy1yZqkTxoTxHCcv+yktmp/e6vkpJtyIf7EFuuo2q8SAr0xDoSxVYWYy8fkikzElvCPALhCkJjoYbXV9IHGkr3x0G1m5fmo5NrTvCt4VeqoTvZdkNgr7IgAv0RwcewwlwE3GeA+h1k+NGcCopZgawCq60kRO00p9mdbjUoX0fH9VxrtCrM5bYSs8AiRZz9YhakU7DQ1NZyKuSk1qME/di0= ravish@thinkpad"
 
 # install and run ssh server
-# sudo apt update && sudo apt install openssh-server && sudo systemctl start ssh
+sudo apt update && sudo apt install openssh-server && sudo systemctl start ssh
 
 # setting up user
 sudo adduser --home /home/git git
