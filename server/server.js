425a06b3cbe678e38cea6c17e8a69313e0f1c18b
ravish0007 <ravish.nfs@gmail.com>
2022-05-06 10:18:49 +0530

    Add server.js: implement basic server

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const { exec } = require('child_process')
+
+require('dotenv').config()
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.post('/new', (req, res) => {
+  if (!req.body.repository) {
+    return res.send(400).json({ message: 'repository not found' })
+  }
+
+  exec(`sshpass -p ${process.env.gitpass} ssh git@${process.env.gitserver} newrepo`, (error, stdout, stderr) => {
+    if (error) {
+      return res.send(500).json({ message: `${error.message}` })
+    }
+    if (stderr) {
+      return res.send(449).json({ message: `${stderr}` })
+    }
+    return res.send(201).json({ message: `${stdout}` })
+  })
+})
+
+app.listen(5000, () => console.log('listening on 5000'))
