bac558fc0615a5bf3ccc936b14ae38d93d8516b9
ravish0007 <ravish.nfs@gmail.com>
2022-05-11 16:37:05 +0530

    Implement verify-user controller

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const cookieSession = require('cookie-session')
+const cookieParser = require('cookie-parser')
 
 const util = require('util')
 const exec = util.promisify(require('child_process').exec)
@@ -12,6 +14,10 @@ const { repoRoute, authRoute } = require('./routes')
 const app = express()
 
 app.use(cors({ origin: process.env.UI_ROOT, credentials: true }))
+
+app.use(cookieSession({ secret: process.env.SESSION_SECRET, httpOnly: true, sameSite: 'strict' }))
+app.use(cookieParser())
+
 app.use(bodyParser.json())
 
 app.use('/api/repo', repoRoute)
