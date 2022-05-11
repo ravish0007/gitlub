0f9329f8860e2c56011dad87a4e7fdfe260e805c
ravish0007 <ravish.nfs@gmail.com>
2022-05-11 14:30:24 +0530

    Modify controller/index.js: add authController

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,3 +1,4 @@
-const repoController = require('./repoController.js')
+const repoController = require('./repoController')
+const authController = require('./authController')
 
-module.exports = {repoController}
+module.exports = { repoController }
