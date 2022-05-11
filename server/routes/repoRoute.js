57e5b60ae53a4216a547eb3a45b5e8cb3242fb80
ravish0007 <ravish.nfs@gmail.com>
2022-05-11 11:53:22 +0530

    Fix gitlog route

--- server/routes/repoRoute.js
+++ server/routes/repoRoute.js
@@ -6,6 +6,6 @@ const router = Router()
 
 router.get('/list', repoController.listRepositories)
 router.post('/new', repoController.createRepository)
-router.get('/log:repository', repoController.sendGitLog)
+router.get('/log/:repository', repoController.sendGitLog)
 
 module.exports = router
