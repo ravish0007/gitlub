7238cd0ec88c1d0af187e95c8123958092470926
ravish0007 <ravish.nfs@gmail.com>
2022-05-19 11:14:33 +0530

    move state and integrate tree viewer

--- client/src/components/Repo.js
+++ client/src/components/Repo.js
@@ -1,39 +1,73 @@
-
 import { useState, useEffect } from 'react'
 
-import axios from 'axios'
+import Tree from './Tree'
+import Code from './Code'
 
-function gitlog (data, repo) {
+import gitlubService from '../gitlubService'
+
+function viewtree (user, tree, repo, appendPwd, backLevel) {
   return (
     <>
-      <p className='clone'>git clone git@192.168.0.110:{repo}</p>
-      <pre className='logs'>
-        {data}
-      </pre>
+      <p className='p-4 rounded-md bg-gray-100 text-slate-500'>
+        git clone {user}@192.168.0.110:{repo}
+      </p>
+
+
+
+      <div className='rounded-md border border-slate-300'>
+        <div
+          className='py-2 px-4 text-slate-500 text-2xl  hover:text-sky-600 w-full hover:bg-gray-100'
+          onClick={backLevel}
+        >
+          ..
+        </div>
+        {Array.isArray(tree)
+          ? (
+            <Tree content={tree} addPath={appendPwd} />
+            )
+          : (
+            <Code content={tree} />
+            )}
+      </div>
     </>
   )
 }
 
-export default function Repo ({ repo }) {
+export default function Repo ({ user, repo }) {
   const [show, setShow] = useState(false)
-  const [data, setData] = useState(false)
+  const [tree, setTree] = useState([])
+  const [pwd, setPwd] = useState('')
+
+  function appendPwd (content) {
+    setPwd(pwd === '' ? content : pwd + '/' + content)
+  }
+
+  function backLevel () {
+    setPwd(pwd.substring(0, pwd.lastIndexOf('/')))
+  }
 
   useEffect(() => {
-    axios.get(`http://192.168.0.110:5000/log/${repo}`).then(result => {
-      console.log(result)
-      setData(result.data.log)
-    }).catch(err => console.log(JSON.stringify(err)))
-  }, [show])
+    if(!show) return
+
+    gitlubService
+      .fetchTree(repo, pwd)
+      .then((result) => {
+        console.log(result.data)
+        setTree(result.data.output)
+      })
+      .catch((err) => console.log(JSON.stringify(err)))
+  }, [pwd, show])
 
   return (
-    <div>
+    <div className='w-1/2 mx-auto mt-10 space-y-4'>
       <p
-        className='repolink'
+        className='font-bold pointer-cursor text-sky-600 text-2xl hover:underline'
         onClick={() => setShow(!show)}
-      > {repo}
+      >
+        {repo}
       </p>
-      {show && gitlog(data, repo)}
+      <div className='border boder-slate-500' />
+      {show && viewtree(user, tree, repo, appendPwd, backLevel)}
     </div>
-
   )
 }
